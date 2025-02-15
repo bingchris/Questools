ec3e773a1f83de422109a343f5fbaf607e7c6638
Tarvey <59284168+Tarvey@users.noreply.github.com>
2025-02-15 13:56:30 +0100

    yaml

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ from sys import version as sysdotversion, platform as sysdotplatform
 from shutil import which
 from subprocess import check_output
 from os import system, path
+try: from yaml import safe_load as yamlload, YAMLError
+except ImportError: print("Install pyyaml from pip to use qtt"); exit(1)
